42950e45ea5d1f8b7758060526d2a42ae08a20f5
Garth Carey <Garthcarey0862@gmail.com>
2024-12-05 19:58:12 -0700

    fixed the location of a hook import and it solved the issue of the hook not being called

--- app/page.js
+++ app/page.js
@@ -1,56 +1,79 @@
-"use client";
-
-import Link from "next/link";
-import { useUserAuth } from "./browse/_utils/auth-context";
+'use client';
+import { useUserAuth } from './browse/_utils/auth-context';
+import Link from 'next/link';
 
 //Home Page
 export default function Page() {
-    console.log(useUserAuth())
+  console.log(useUserAuth());
 
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
-    const signIn = async () => {
-        await gitHubSignIn();
-    }
+  const signIn = async () => {
+    await gitHubSignIn();
+  };
 
-    const signOut = async () => {
-        await firebaseSignOut();
-    }
+  const signOut = async () => {
+    await firebaseSignOut();
+  };
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <header className="bg-purple-500 text-white p-8 shadow-md">
-                <div className="container mx-auto flex justify-between items-center">
-                    <h1 className="text-3xl font-bold">Bookish Buzz</h1>
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <header className='bg-purple-500 text-white p-8 shadow-md'>
+        <div className='container mx-auto flex justify-between items-center'>
+          <h1 className='text-3xl font-bold'>Bookish Buzz</h1>
 
-                    <ul className="flex space-x-4">
-                        <li><Link href="http://localhost:3000/" className="hover:underline">Home</Link></li>
-                        <li><Link href="/browse" className="hover:underline">Browse</Link></li>
-                        <li><button className="mb-4 hover:underline" onClick={signIn}>Sign In</button></li>
-                    </ul>
-
-                </div>
-            </header>
-
-            <main className="flex-grow p-6">
-                {user ? (
-                    <div className="flex flex-col items-center justify-center h-screen">
-                    <p className="mb-4 p-4">Welcome, {user.displayName}</p>
-                    <button className="mb-4 hover:underline" onClick={signOut}>Sign Out</button>
-                    <p className="text-center p-8">welcome message to the site!</p>
-                </div>
-            ) : (
-                <div>
-                    <h1 className="font-cursive text-3xl text-center p-4">Sorry, you need to sign in first!</h1>
-                    <button className="hover:underline" onClick={signIn}>Sign In</button>
-                </div>
-            )}
-            </main>
-
-            <footer className="bg-purple-500 text-white p-12 shadow-md">
-
-            </footer>
-
+          <ul className='flex space-x-4'>
+            <li>
+              <Link
+                href='http://localhost:3000/'
+                className='hover:underline'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/browse'
+                className='hover:underline'>
+                Browse
+              </Link>
+            </li>
+            <li>
+              <button
+                className='mb-4 hover:underline'
+                onClick={signIn}>
+                Sign In
+              </button>
+            </li>
+          </ul>
         </div>
-    )
+      </header>
+
+      <main className='flex-grow p-6'>
+        {user ? (
+          <div className='flex flex-col items-center justify-center h-screen'>
+            <p className='mb-4 p-4'>Welcome, {user.displayName}</p>
+            <button
+              className='mb-4 hover:underline'
+              onClick={signOut}>
+              Sign Out
+            </button>
+            <p className='text-center p-8'>welcome message to the site!</p>
+          </div>
+        ) : (
+          <div>
+            <h1 className='font-cursive text-3xl text-center p-4'>
+              Sorry, you need to sign in first!
+            </h1>
+            <button
+              className='hover:underline'
+              onClick={signIn}>
+              Sign In
+            </button>
+          </div>
+        )}
+      </main>
+
+      <footer className='bg-purple-500 text-white p-12 shadow-md'></footer>
+    </div>
+  );
 }
